648f3b1575443943a4445f3d54b4ff54d6b789ed
Saadi-Rahman <sadi6280@gmail.com>
2022-11-11 16:34:49 +0600

    initial server setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+// require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) =>{
+    res.send('Mr. Locker server is running..');
+});
+
+app.listen(port, () =>{
+    console.log(`Mr. Locker Server running on port: ${port}`);
+});
